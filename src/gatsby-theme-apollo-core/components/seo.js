1eefcf2e8eb6c6e08f0e3a5a09d183ea3a7b5878
Mohammed Modi <mohammed.modi@loginradius.com>
2020-08-21 14:44:20 +0530

    Fixed Preview Image

--- src/gatsby-theme-apollo-core/components/seo.js
+++ src/gatsby-theme-apollo-core/components/seo.js
@@ -2,29 +2,30 @@ import PropTypes from "prop-types";
 import React from "react";
 import { Helmet } from "react-helmet";
 
-import defaultImg from "../../../static/lr-logo.svg"
+import defaultImg from "../../../static/preview.svg";
 
 import { withPrefix } from "gatsby";
 
 export default function SEO(props) {
-  const {
-    title,
-    description,
-    siteName,
-    twitterCard,
-    children,
-  } = props;
+  const { title, description, siteName, twitterCard, children } = props;
   return (
     <Helmet>
       <title>{title}</title>
+      <link rel="icon" href={withPrefix("/favicon.png")} />
+
+      <meta name="title" content={title} />
+      <meta name="description" content={description} />
+
       <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
       <meta property="og:image" content={defaultImg} />
       <meta property="og:site_name" content={siteName} />
-      <meta property="og:description" content={description} />
-      <meta name="twitter:card" content={twitterCard} />
-      <meta name="twitter:title" content={title} />
-      <meta name="twitter:description" content={description} />
-      <link rel="icon" href={withPrefix("/favicon.png")} />
+
+      <meta property="twitter:card" content={twitterCard} />
+      <meta property="twitter:title" content={title} />
+      <meta property="twitter:description" content={description} />
+      <meta property="twitter:image" content={defaultImg} />
+
       {children}
     </Helmet>
   );
@@ -39,5 +40,5 @@ SEO.propTypes = {
 };
 
 SEO.defaultProps = {
-  twitterCard: "summary",
+  twitterCard: "summary_large_image",
 };
