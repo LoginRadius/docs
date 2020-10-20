c19a0eef3106577a1e92d20161c0411cabbea970
HemantL04 <73017628+HemantL04@users.noreply.github.com>
2020-10-20 13:33:27 +0530

    Update theme-options.js

--- theme-options.js
+++ theme-options.js
@@ -4,25 +4,25 @@ const navConfig = {
     description:
       "A quick way to implement the authentication mechanism in your application",
     omitLandingPage: true,
-    isExternal: false,
+    isExternal: false
   },
   "API Reference": {
     url:
       "https://www.loginradius.com/docs/developer/api/",
     description: "Reference of all the REST APIs which you can use",
-    isExternal: true,
+    isExternal: true
   },
   "How to": {
     url: "/howto/email-smtp-config/",
     description:
       "Once you are finished with basic authentication, there are other things to add in your application",
-    isExternal: false,
+    isExternal: false
   },
   FAQ: {
     url: "/faq/api-credentials/",
     description:
       "List of all the nitty-gritty questions you may encounter during your implimentation",
-    isExternal: false,
+    isExternal: false
   },
 };
 
@@ -30,17 +30,17 @@ const footerNavConfig = {
   Blog: {
     href: "https://www.loginradius.com/engineering/blog/",
     target: "_blank",
-    rel: "noopener noreferrer",
+    rel: "noopener noreferrer"
   },
   Contribute: {
     href: "https://github.com/LoginRadius/",
     target: "_blank",
-    rel: "noopener noreferrer",
+    rel: "noopener noreferrer"
   },
   Dashboard: {
     href: "https://dashboard.loginradius.com/",
     target: "_blank",
-    rel: "noopener noreferrer",
+    rel: "noopener noreferrer"
   },
 };
 
