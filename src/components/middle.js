342714bccdd59c002d2653d852e9dc2b19c68429
sanchoksc <sandeepchowdaryksc@gmail.com>
2021-02-10 14:19:03 +0530

    landing page

---
+++ src/components/middle.js
@@ -0,0 +1,373 @@
+import React from "react"
+
+const Middle =() => { 
+    return (
+        <main>
+        <section class="hero py-72">
+          <div class="grid-50 d-flex align-items-center">
+            <div>
+              <h1>Identity Platform Documents</h1>
+              <p>
+                Learn how to implement the LoginRadius Identity Platform for
+                your application. Get the comprehensive guide including
+                quick-starts, implementation tutorials, example code, features
+                and more.
+              </p>
+              <p>
+                You can find everything from SDKs in your favorite languages to
+                APIs and sample demos using various technologies.
+              </p>
+              <div class="btn-group">
+                <a href="#" class="btn btn-primary">Try 5 minute Setup Guide</a>
+                <a href="#" class="btn btn-outline">Sign up for free</a>
+              </div>
+            </div>
+            <div class="wrapper">
+              <div class="steps" id="steps">
+                <div class="step step1">
+                  <div class="number">
+                    <img src="assets/images/step1.png" width="36" />
+                  </div>
+                  <div class="info">
+                    <p class="title">
+                      Create your account <span>(it’s free)</span>
+                    </p>
+                  </div>
+                </div>
+
+                <div class="step step2">
+                  <div class="number">
+                    <img src="assets/images/step2.png" width="36" />
+                  </div>
+                  <div class="info">
+                    <p class="title">Setup Your App</p>
+                  </div>
+                </div>
+                <div class="step step3">
+                  <div class="number">
+                    <img src="assets/images/step3.png" width="36" />
+                  </div>
+                  <div class="info">
+                    <p class="title">Get Started</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section class="tutorials py-72">
+          <div class="section-header">
+            <h2>Tutorials</h2>
+            <p>
+              Explore our code and resources to get started in just few minutes.
+            </p>
+          </div>
+          <div class="grid-25">
+            <div class="card">
+              <div class="icon">
+                <img src="assets/images/single-page-app.svg" />
+              </div>
+              <h3 class="title">Single Page App</h3>
+              <ul>
+                <li>
+                  <a href="#">
+                    <img src="assets/images/react.svg" width="20" />
+                    <span>React</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <img src="assets/images/angular.svg" width="20" />
+                    <span>Angular</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <img src="assets/images/vue-dot-js.svg" width="20" />
+                    <span>VueJs</span>
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div class="card">
+              <div class="icon">
+                <img src="assets/images/web-apps.svg" />
+              </div>
+              <h3 class="title">Web Apps</h3>
+              <ul>
+                <li>
+                  <a href="#">
+                    <img src="assets/images/node-dot-js.svg" width="20" />
+                    <span>NodeJs</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <img src="assets/images/php.svg" width="20" />
+                    <span>PHP</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <img src="assets/images/java.svg" width="20" />
+                    <span>JAVA</span>
+                  </a>
+                </li>
+              </ul>
+              <div class="read-more">
+                <a href="#">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="#008ecf"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    class="feather feather-plus"
+                  >
+                    <line x1="12" y1="5" x2="12" y2="19"></line>
+                    <line x1="5" y1="12" x2="19" y2="12"></line>
+                  </svg>
+                  <span>More</span></a
+                >
+              </div>
+            </div>
+            <div class="card">
+              <div class="icon">
+                <img src="assets/images/mobile-apps.svg" />
+              </div>
+              <h3 class="title">Mobile Apps</h3>
+              <ul>
+                <li>
+                  <a href="#">
+                    <img src="assets/images/android.svg" width="20" />
+                    <span>Android</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <img src="assets/images/ios.svg" width="20" />
+                    <span>iOS</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <img src="assets/images/adobephonegap.svg" width="20" />
+                    <span>PhoneGap</span>
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div class="card">
+              <div class="icon">
+                <img src="assets/images/cms-app.svg" />
+              </div>
+              <h3 class="title">CMS App</h3>
+              <ul>
+                <li>
+                  <a href="#">
+                    <img src="assets/images/wordpress.svg" width="20" />
+                    <span>WordPress</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <img src="assets/images/drupal.svg" width="20" />
+                    <span>Drupal </span>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <img src="assets/images/joomla.svg" width="20" />
+                    <span>Joomla</span>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </section>
+        <section class="resources py-72">
+          <div class="section-header">
+            <h2>Recommended Resources</h2>
+            <p>
+              Whether you are a beginner or an experienced professional, our
+              hands-on approach helps you arrive at your goals faster, with more
+              confidence and at your own pace.
+            </p>
+          </div>
+          <div class="grid-50">
+            <div class="card">
+              <div class="icon">
+                <img src="assets/images/login-methods.svg" />
+              </div>
+              <h3 class="title">Login Methods we offer</h3>
+              <ul>
+                <li>
+                  <a href="#">Email/Password Login</a>
+                </li>
+                <li>
+                  <a href="#">Social Login</a>
+                </li>
+                <li>
+                  <a href="#">Phone Login</a>
+                </li>
+                <li>
+                  <a href="#">Passwordless Login</a>
+                </li>
+              </ul>
+            </div>
+            <div class="card">
+              <div class="icon">
+                <img src="assets/images/user-managment.svg" />
+              </div>
+              <h3 class="title">Explore user managment options</h3>
+              <ul>
+                <li>
+                  <a href="#">Mange Users</a>
+                </li>
+                <li>
+                  <a href="#">Migrate User Data</a>
+                </li>
+                <li>
+                  <a href="#">Manage Roles</a>
+                </li>
+              </ul>
+            </div>
+            <div class="card">
+              <div class="icon">
+                <img src="assets/images/branding.svg" />
+              </div>
+              <h3 class="title">Personalize interfaces and branding</h3>
+              <ul>
+                <li>
+                  <a href="#">Customize Auth Page (IDX)</a>
+                </li>
+                <li>
+                  <a href="#">Advanced Editor </a>
+                </li>
+                <li>
+                  <a href="#">Change Email Templates</a>
+                </li>
+                <li>
+                  <a href="#">Add Fields to Registartion</a>
+                </li>
+                <li>
+                  <a href="#">Setup custom domain </a>
+                </li>
+              </ul>
+            </div>
+            <div class="card">
+              <div class="icon">
+                <img src="assets/images/sso.svg" />
+              </div>
+              <h3 class="title">Single Sign On</h3>
+              <ul>
+                <li>
+                  <a href="#">Web SSO </a>
+                </li>
+                <li>
+                  <a href="#">Federated SSO </a>
+                </li>
+              </ul>
+            </div>
+            <div class="card">
+              <div class="icon">
+                <img src="assets/images/intigration.svg" />
+              </div>
+              <h3 class="title">Integrate Third Parties</h3>
+              <ul>
+                <li>
+                  <a href="#">Setup Web Hooks </a>
+                </li>
+                <li>
+                  <a href="#">Connect SAML App </a>
+                </li>
+                <li>
+                  <a href="#"> Connect JWT App</a>
+                </li>
+                <li>
+                  <a href="#"> Connect OAuth App</a>
+                </li>
+                <li>
+                  <a href="#">Connect OIDC App </a>
+                </li>
+                <li>
+                  <a href="#">Integrate BigCommerce</a>
+                </li>
+                <li>
+                  <a href="#">Integrate Shopify</a>
+                </li>
+              </ul>
+            </div>
+            <div class="card">
+              <div class="icon">
+                <img src="assets/images/secure-authentication.svg" />
+              </div>
+              <h3 class="title">Secure Authentication</h3>
+              <ul>
+                <li>
+                  <a href="#">Google reCAPTCHA </a>
+                </li>
+                <li>
+                  <a href="#">Secure Against Bruteforce </a>
+                </li>
+                <li>
+                  <a href="#"> Set Strong Password policy</a>
+                </li>
+                <li>
+                  <a href="#"> Multi Factor Authentication</a>
+                </li>
+                <li>
+                  <a href="#"> Change Session Lifetime </a>
+                </li>
+                <li>
+                  <a href="#">Enable Force Logout</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </section>
+        <section class="resources comprehensive-resources py-72">
+          <div class="section-header">
+            <h2>Comprehensive Resources</h2>
+            <p>
+              Check out concepts, SDKs, and APIs to extensively use LoginRadius
+              Identity Platform.
+            </p>
+          </div>
+          <div class="grid-50">
+            <div class="card">
+              <div class="icon">
+                <img src="assets/images/concepts.svg" />
+              </div>
+              <h3 class="title">Concepts</h3>
+              <ul>
+                <li>
+                  <a href="#">IDX</a>
+                </li>
+              </ul>
+            </div>
+            <div class="card">
+              <div class="icon">
+                <img src="assets/images/references.svg" />
+              </div>
+              <h3 class="title">References</h3>
+              <ul>
+                <li>
+                  <a href="#">SDKs </a>
+                </li>
+                <li>
+                  <a href="#">APIs</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </section>
+    </main>
+    )
+}
+
+export default Middle 
