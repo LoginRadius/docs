e871b4567aba83c85460a82ee4f0fcc7ebe96b9f
abhimanyu singh rathore <abhimanyu.rathore@loginradius.com>
2021-02-12 16:16:44 +0530

    PCA-5071 update favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -66,7 +66,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/favicon.ico`,
+        icon: `content/assets/logo.svg`,
       },
     },
     `gatsby-plugin-react-helmet`,
