7dd4b1d500d4999c174299d0fd169575cbdf0dd0
HiteshKumawat <hitesh.kumawat@loginradius.com>
2021-02-19 19:17:30 +0530

    Landing page resposive

--- src/components/header.js
+++ src/components/header.js
@@ -1,28 +1,38 @@
 import React from "react"
 import { Link } from "gatsby"
 
-const Header =() => { 
-    return ( 
-      <header>
-        <div>
+const Header = () => {
+  return (
+    <header>
+      <div>
         <div>
           <div className="logo">
-          <Link to="/">
-            <img src="images/logo.svg" width="228" />
+            <Link to="/">
+              <img src="images/logo.svg" width="228" />
             </Link>
           </div>
-          <div className="search">
+          <div className="search hide-mobile">
             <form>
               <input type="text" placeholder="Search documentation..." />
             </form>
           </div>
         </div>
         <div className="authentication-buttons">
-          <a href="#" className="btn btn-secondary">Login</a>
-          <a href="#" className="btn btn-primary">Sign up for free</a>
+          <a href="#" className="btn btn-secondary">
+            Login
+          </a>
+          <a href="#" className="btn btn-primary">
+            Sign up for free
+          </a>
+        </div>
+        <div className="search hide-desktop">
+          <form>
+            <input type="text" placeholder="Search documentation..." />
+          </form>
         </div>
       </div>
-      </header> )
+    </header>
+  )
 }
 
-export default Header 
+export default Header
