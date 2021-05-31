72bfd2783985f80eb1fdfab604b1d9cd72c9737a
HiteshKumawat <hitesh.kumawat@loginradius.com>
2021-05-31 11:12:03 +0530

    Third party integration UI

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,7 +5,7 @@ import "typeface-merriweather"
 import "./src/normalize.css"
 // custom CSS styles
 //import "./src/style.css"
-import "./assets/css/base.min.css"
+import "./static/css/base.min.css"
 
 // Highlighting for code blocks
 import "prismjs/themes/prism.css"
