0ce825f6ffe42995693e46844c602376d2b3d22b
Hitesh <hiteshkumawat19@gmail.com>
2022-01-25 14:22:28 +0530

    doc search result UI (#415)

--- src/components/search/index.js
+++ src/components/search/index.js
@@ -1,13 +1,11 @@
 import algoliasearch from "algoliasearch/lite"
-import { createRef, default as React, useState, useMemo } from "react";
+import { createRef, default as React, useState, useMemo } from "react"
 import { InstantSearch } from "react-instantsearch-dom"
 
 import SearchBox from "./search-box"
 import StyledSearchResult from "./styled-search-result"
 import useClickOutside from "./use-click-outside"
-
-
-
+import styles from "./search.module.css"
 const Search = ({ indices }) => {
   const rootRef = createRef()
   const [query, setQuery] = useState()
@@ -32,12 +30,13 @@ const Search = ({ indices }) => {
       >
         <SearchBox onFocus={() => setFocus(true)} hasFocus={hasFocus} />
         <StyledSearchResult
+          className={styles.searchResult}
           show={query && query.length > 0 && hasFocus}
           indices={indices}
         />
       </InstantSearch>
     </div>
-  );
+  )
 }
 
-export default Search;
+export default Search
