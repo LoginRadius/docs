4b24f9a64ef7447619b96f77d562c3796eb169a6
Versha <versha.gupta@loginradius.com>
2022-04-18 17:33:53 +0530

    Popup added

--- src/components/container.js
+++ src/components/container.js
@@ -8,7 +8,7 @@ const Middle = () => {
   const sdkRef = React.createRef()
   const gotQRef = React.createRef()
   const [display, setDisplay] = useState(false)
-
+  const [openModel, setOpenModel] = useState(true)
   useEffect(() => {
     if (
       window &&
@@ -27,12 +27,54 @@ const Middle = () => {
         gotQRef.current.scrollIntoView({ block: "start" })
       }
     }
+    let modelShown = localStorage.getItem("docsModelShown");
+    if(modelShown && modelShown > new Date()){
+      setOpenModel(false);
+    }
   })
 
   return (
     <main>
       {/*Hero section*/}
       <section className="hero py-72">
+      <Modal isOpen={openModel} ariaHideApp={false}>
+        <div className="card no-shadow p-0">
+         
+          <div className="popup-content">
+          <a onClick={()=>{
+             var expires = new Date();
+             expires = expires.setHours(expires.getHours() + 24);
+             localStorage.setItem("docsModelShown", expires)
+            setOpenModel(false);
+          }}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="feather feather-x"
+              >
+                <line x1="18" y1="6" x2="6" y2="18"></line>
+                <line x1="6" y1="6" x2="18" y2="18"></line>
+              </svg>
+            </a>
+            
+            <Link to="https://www.loginradius.com/docs/" className="btn btn-secondary">
+            Move to Enterprise Docs
+              </Link>
+              <Link onClick={()=>{
+setOpenModel(false);
+              }} className="btn btn-primary">
+              Stay at Developer Docs
+              </Link>
+          </div>
+        </div>
+      </Modal>
         <div className="grid-50 d-flex align-items-end justify-content-between">
           <div>
             <h1>LoginRadius Documentation</h1>
